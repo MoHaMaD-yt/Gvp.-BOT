9f1a04d6031e9090e9793bfcb8c26d775275d683
aliexers <107118239+aliexers@users.noreply.github.com>
2022-06-29 17:10:05 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,40 +60,6 @@ def hasInsult(msg):
 		else: continue
 	return swData
 
-def alert(guid,user,alert_text=""):
-	no_alerts.append(guid)
-	alert_count = int(no_alerts.count(guid))
-
-	alerts[user] = alert_count
-
-	max_alert = 5    # you can change it
-
-
-	if alert_count == max_alert:
-		blacklist.append(guid)
-		bot.sendMessage(target, "\n 🚫 کاربر [ @"+user+" ] \n ("+ str(max_alert) +") اخطار دریافت کرد ، بنابراین اکنون اخراج میشود .", msg["message_id"])
-		bot.banGroupMember(target, guid)
-		return
-
-	for i in range(max_alert):
-		no = i+1
-		if alert_count == no:
-			bot.sendMessage(target, "🔻اخطار [ @"+user+" ] \n\n"+ str(alert_text) +" شما ("+ str(no) +"/"+ str(max_alert) +") اخطار دریافت کرده اید .\n\nپس از دریافت "+ str(max_alert) +" اخطار ، از گروه اخراج خواهید شد .", msg["message_id"])
-			return
-		
-def hasAds(msg):
-	links = list(map(lambda ID: ID.strip()[1:],findall("@[\w|_|\d]+", msg))) + list(map(lambda link:link.split("/")[-1],findall("rubika\.ir/\w+",msg)))
-	joincORjoing = "joing" in msg or "joinc" in msg
-
-	if joincORjoing: return joincORjoing
-	else:
-	for link in links:
-			try:
-				Type = bot.getInfoByUsername(link)["data"]["chat"]["abs_object"]["type"]
-			   if Type == "Channel":
-				return True
-		except KeyError: return False
-
 def search_i(text,chat,bot):
     try:
         search = text[11:-1]
